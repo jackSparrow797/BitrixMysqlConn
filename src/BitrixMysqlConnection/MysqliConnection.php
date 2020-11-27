cc609ac382c311e872e4f4b56550b1f588a858ca
Медведев Евгений <medvedev@adv.ru>
2020-11-27 10:05:11 +0400

    add reconnect

--- src/BitrixMysqlConnection/MysqliConnection.php
+++ src/BitrixMysqlConnection/MysqliConnection.php
@@ -1,10 +1,61 @@
 <?php
 
-namespace Adv\BitrixMysqlConnection;
+namespace Jack797\BitrixMysqlConnection;
 
 use Bitrix\Main\DB\MysqliConnection as BaseMysqliConnection;
+use Bitrix\Main\DB\SqlQueryException;
 
 class MysqliConnection extends BaseMysqliConnection
 {
+    use Reconnect;
+    /*********************************************************
+     * Query
+     *********************************************************/
+
+    /**
+     * Executes a query against connected database.
+     * Rises SqlQueryException on any database error.
+     * <p>
+     * When object $trackerQuery passed then calls its startQuery and finishQuery
+     * methods before and after query execution.
+     *
+     * @param string                            $sql          Sql query.
+     * @param array                             $binds        Array of binds.
+     * @param \Bitrix\Main\Diag\SqlTrackerQuery $trackerQuery Debug collector object.
+     *
+     * @throws \Bitrix\Main\Db\SqlQueryException
+     * @return resource
+     */
+    protected function queryInternal($sql, array $binds = null, \Bitrix\Main\Diag\SqlTrackerQuery $trackerQuery = null)
+    {
+        $this->connectInternal();
+
+        if ($trackerQuery != null) {
+            $trackerQuery->startQuery($sql, $binds);
+        }
+
+        /** @var $con \mysqli */
+        $con = $this->resource;
+        $result = $con->query($sql, MYSQLI_STORE_RESULT);
+
+        if ($trackerQuery != null) {
+            $trackerQuery->finishQuery();
+        }
+
+        $this->lastQueryResult = $result;
+
+        if (!$result) {
+            if ($this->isNeedReconnect() && $this->isNeedTryConnect()) {
+                $this->newAttempt();
+                $result = $this->queryInternal($sql, $binds, $trackerQuery);
+            } else {
+                throw new SqlQueryException('Mysql query error', $this->getErrorMessage(), $sql);
+            }
+        }
+
+        return $result;
+    }
+
+
 
 }
---
+++ src/BitrixMysqlConnection/Reconnect.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jack797\BitrixMysqlConnection;
+
+trait Reconnect
+{
+    protected $reconnectErrors = [2006];
+
+    /** @var int */
+    protected $maxAttempts = 5;
+
+    /** @var int */
+    protected $attempts = 0;
+
+    /**
+     * @return bool
+     */
+    protected function isNeedTryConnect(): bool
+    {
+        return $this->maxAttempts < $this->attempts;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isNeedReconnect(): bool
+    {
+        /** @var $con \mysqli */
+        $con = $this->resource;
+        return in_array($con->errno, $this->reconnectErrors, true);
+    }
+
+
+    private function newAttempt(): void
+    {
+        $this->attempts++;
+    }
+}
